3d0d7900505d23a6e7eee8da44ff0f945613ba7a
oversleep-t <decimaltsukumo@gmail.com>
2020-03-06 01:37:19 +0900

    Revert "画像ダウンロード"
    This reverts commit bb36cd7b575acc65f938e6406540db3f25bf0144.

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
 # インストールした discord.py を読み込む
-import discord, json, os, urllib, shutil
+import discord, json
 from requests_oauthlib import OAuth1Session
 from config import discordpy_token , e_channel_id , e_archive_channel_id, consumer_key, consumer_secret, access_token, access_token_secret
 
@@ -19,18 +19,6 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
-location = "~/e-archive"
-tmp_name = "/tmp"
-dir_name = "/dir"
-
-def dir_check():
-    if not os.path.isdir(tmp_name):
-        os.mkdir(tmp_name)
-    count = 0
-    while not os.path.isdir(tmp_name + dir_name + str(count)):
-        dir_name = dir_name + str(count)
-        os.mkdir(tmp_name + dir_name)
-        count += 1
 
 # 起動時に動作する処理
 @client.event
@@ -41,10 +29,6 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    global image
-    global image_number
-    dir_check()
-    image_number = 0
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
@@ -56,26 +40,13 @@ async def on_message(message):
 
         e_archive_channel = client.get_channel(E_ARCHIVE_CHANNEL_ID)
         if 'twitter.com' in message.content:
-            tweetId = '1233407306781429760'
+            tweetId = '1231419383605420032'
             url = 'https://api.twitter.com/1.1/statuses/show.json?id=' + tweetId
             req = twitter.get(url)
             if req.status_code == 200:
                 result = json.loads(req.text)
                 archiveText = result['user']['name'] + '\n' + '@' + result['user']['screen_name'] + '\n' + result['text'] + '\n' + result['created_at']
-                media_list = result['extended_entities']['media']
                 await e_archive_channel.send(archiveText)
-
-                for media in media_list:
-                    image = media['media_url']
-                    filepath = tmp_name + dir_name + "/image_" + os.path.basename(image)
-                    # 画像をダウンロード
-                    with open(filepath, 'wb') as f:
-                        img = urllib.request.urlopen(image).read()
-                        f.write(img)
-                        await e_archive_channel.send(filepath)
-                        # 画像をdiscordに送信
-                        # await client.send_file(E_CHANNEL_ID, 画像)
-                shutil.rmtree(os.path.join(location, tmp_name))
             else:
                 await message.channel.send('ツイート取得に失敗しました')
 
