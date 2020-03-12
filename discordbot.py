b7b085792d3a8afa76a58265488cdabe9f6a6a4c
enzounderworld <enzounderworld@gmail.com>
2020-03-12 23:15:57 +0900

    関数作成　URL直貼り.複数URLに対応

--- discordbot.py
+++ discordbot.py
@@ -21,6 +21,47 @@ def dir_check():
     if not os.path.isdir(MEDIA_DIR):
         os.mkdir(MEDIA_DIR)
 
+def find_url(msg):
+    pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
+    url_list = re.findall(pattern, msg)
+    return url_list
+
+def is_valid_url(url):
+    try:
+        f = urllib.request.urlopen(url)
+        f.close()
+        return True
+    except:
+        return False
+
+
+async def tw(url , archive_ch):
+    pattern = '/status/'
+    split_list = re.split(pattern, url)
+    tweetId = re.search(r'\d+', split_list[1]).group()
+
+    api_url = 'https://api.twitter.com/1.1/statuses/show.json?id=' + tweetId
+    req = twitter.get(api_url)
+    if req.status_code == 200:
+        result = json.loads(req.text)
+        archiveText = result['user']['name'] + '\n' + '@' + result['user']['screen_name'] + '\n' + result['text'] + '\n' + result['created_at']
+        await archive_ch.send(archiveText)
+
+        if result.get('extended_entities') is not None:
+            media_list = result['extended_entities']['media']
+            dir_check()
+
+            for media in media_list:
+                image = media['media_url']
+                timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+                filepath = FILEPATH + timestamp + '.jpg'
+                # 画像をダウンロード
+                with open(filepath, 'wb') as f:
+                    img = urllib.request.urlopen(image).read()
+                    f.write(img)
+                    # 画像をdiscordに送信
+                    await archive_ch.send(file=discord.File(filepath))   
+
 
 # 起動時に動作する処理
 @client.event
@@ -43,42 +84,27 @@ async def on_message(message):
         msg = message.content
         attachments = message.attachments
     
-        e_archive_channel = client.get_channel(E_ARCHIVE_CHANNEL_ID)
-        if 'twitter.com' in msg:
+        archive_ch = client.get_channel(E_ARCHIVE_CHANNEL_ID)
 
-            pattern = '/status/'
-            split_list = re.split(pattern, msg)
-            tweetId = re.search(r'\d+', split_list[1]).group()
+        url_list = find_url(msg)
 
-            url = 'https://api.twitter.com/1.1/statuses/show.json?id=' + tweetId
-            req = twitter.get(url)
-            if req.status_code == 200:
-                result = json.loads(req.text)
-                archiveText = result['user']['name'] + '\n' + '@' + result['user']['screen_name'] + '\n' + result['text'] + '\n' + result['created_at']
-                await e_archive_channel.send(archiveText)
+        for url in url_list:
+            if not is_valid_url(url):
+                print('無効なURL')
+                continue
 
-                media_list = result['extended_entities']['media']
-                dir_check()
-
-                for media in media_list:
-                    image = media['media_url']
-                    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-                    filepath = FILEPATH + timestamp + '.jpg'
-                    # 画像をダウンロード
-                    with open(filepath, 'wb') as f:
-                        img = urllib.request.urlopen(image).read()
-                        f.write(img)
-                        # 画像をdiscordに送信
-                        await e_archive_channel.send(file=discord.File(filepath))
-
+            if 'twitter.com' in url:
+                await tw(url , archive_ch)
             else:
-                await message.channel.send('ツイート取得に失敗しました')
-    
+                #URL直貼りの場合
+                await archive_ch.send(url)
+
+        
         #画像の直貼りの場合
         if attachments:
             for attachment in attachments:
                 file_attachment = await attachment.to_file()
-                await e_archive_channel.send(file=file_attachment)
+                await archive_ch.send(file=file_attachment)
 
 # Botの起動とDiscordサーバーへの接続
 client.run(DISCORDPY_TOKEN)
